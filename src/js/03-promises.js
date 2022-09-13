be3690b30204aaf389a9b76efe6b76690256e856
Volkova Valeria <vvolk.valeria@gmail.com>
2022-09-14 02:53:23 +0300

    done 02-timer

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,17 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+
+
+
+
+
+
+
+
+// console.log(refs.timerEl);
+// console.log(refs.timerEl.target);
+// // formObj[e.target.name] = e.target.value;
+// // console.log(refs.timerEl.elements[key].value);
